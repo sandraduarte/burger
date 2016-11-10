fcf93bd0ebb564663ff758d1510e7e08ae9917f7
sandraduarte <sandramduarte83@gmail.com>
2016-11-10 18:17:32 -0500

    submit

--- server.js
+++ server.js
@@ -5,32 +5,13 @@ var bodyParser = require('body-parser');
 //express config
 var express = require('express');
 var app = express();
-var PORT = process.env.PORT || 3306;
-
-// Adding in the components of the mysql library. Outlining the parameters tied to the mysql db
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-	host: 'localhost',
-	user: 'root',
-	password: '',
-	database: 'burgers_db'
-});
-
-// Connecting the mysql database mentioned above
-connection.connect(function (err) {
-	if (err) {
-		console.error('error connecting: ' + err.stack);
-		return;
-	}
-	console.log('connected as id ' + connection.threadId);
-});
 
 // BodyParser makes it easy for our server to interpret data sent to it.
 // The code below is pretty standard.
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
-app.use(bodyParser.json({type:'application/vnd.api+json'}));
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 // ==============================================================================
 // LISTENER
@@ -38,5 +19,6 @@ app.use(bodyParser.json({type:'application/vnd.api+json'}));
 // ==============================================================================
 
 app.listen(PORT, function() {
-	console.log("App listening on PORT: " + PORT);
+    console.log("App listening on PORT: " + PORT);
 });
+
